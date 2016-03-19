4e0b2d8bc505db8f47016b7e3b515800e51753bb
Sergey Bondarenko <sergibondarenko@gmail.com>
2016-03-19 19:09:15 +0000

    Add bones for the future controller.

---
+++ public/js/controllers/videoloop.js
@@ -0,0 +1,16 @@
+/*global angular*/
+angular.module('videoloopController', [])
+
+.controller('videoloopController', ['$scope', 'Videolinks', function($scope, Videolinks) {
+	$scope.formData = {};
+	$scope.loading = true;
+
+	// GET =====================================================================
+	// when landing on the page, get all todos and show them
+	// use the service to get all the todos
+	Videolinks.get()
+		.success(function(data) {
+			$scope.videolinks = data;
+		});
+
+}]);
