815971ae2af0e35a6b8180730e88ac216bd465d4
Sergey Bondarenko <sergibondarenko@gmail.com>
2016-03-28 23:51:24 +0200

    Add playlist functionality.

--- public/js/controllers/videoloop.js
+++ public/js/controllers/videoloop.js
@@ -3,21 +3,35 @@
 angular.module('videoloopController', [])
 
 .controller('videoloopController', ['$scope', 'Videolinks', function($scope, Videolinks) {
-  
-  var video = document.getElementById("video-el");
 
-  var playVideo = function(videoSrc) {
-    video.setAttribute('src', videoSrc);
-    video.load();
-    video.play();
-  };
-
-  //video.addEventListener("ended", playVideo($scope.videolinks[5].file), false);
   // Get video files and play them   
   Videolinks.get().success(function(data) {
+ 
     $scope.videolinks = data;
 
-    playVideo($scope.videolinks[0].file);
+    var currVideoId = 0;
+    var videoArrLen = $scope.videolinks.length;
+    var video = document.getElementById("video-el");
+
+    // Load and play video
+    var playVideo = function(index) {
+      video.setAttribute('src', $scope.videolinks[index].file);
+      video.load();
+      video.play();
+      console.log($scope.videolinks[index].file);
+    };
+
+    // Play first video
+    playVideo(currVideoId);
+
+    // Play other videos
+    video.addEventListener('ended', function() {
+      currVideoId++;
+      if (currVideoId >= videoArrLen) {
+        currVideoId = 0;
+      }
+      playVideo(currVideoId);
+    });
 
   });
 
