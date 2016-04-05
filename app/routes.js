f5c14502220b9ad05a451668b7f14a5f5a7b16a6
Sergey Bondarenko <sergibondarenko@gmail.com>
2016-04-05 23:11:19 +0200

    Refactor nodejs routes.

--- app/routes.js
+++ app/routes.js
@@ -1,96 +1,67 @@
 var path = require('path');
-var Todo = require('./models/todo');
 var Videolink = require('./models/fonotvdb');
 
 var mongodb = require('mongodb');
-var ObjectId = require('mongodb').ObjectID;
-
-function getTodos(res){
-	Todo.find(function(err, todos) {
-
-			// if there is an error retrieving, send the error. nothing after res.send(err) will execute
-			if (err)
-				res.send(err);
-
-			res.json(todos); // return all todos in JSON format
-		});
-};
+var ObjectId = mongodb.ObjectID;
 
 // Get links
 function getLinks(res){
 	Videolink.find(function(err, hyperlinks) {
 
 			// if there is an error retrieving, send the error. nothing after res.send(err) will execute
-			if (err)
+			if (err) {
 				res.send(err);
+      }
 
-			res.json(hyperlinks); // return all todos in JSON format
+			res.json(hyperlinks); // return all videos in JSON format
 		});
-};
+}
 
 module.exports = function(app) {
 
 	// api ---------------------------------------------------------------------
-	// get all todos
-	app.get('/api/todos', function(req, res) {
-
-		// use mongoose to get all todos in the database
-		getTodos(res);
-	});
 
 	app.get('/api/videolinks', function(req, res) {
 
-		// use mongoose to get all todos in the database
+		// use mongoose to get all videos in the database
 		getLinks(res);
 	});
 
-	// delete a todo
+	// delete a video
 	app.delete('/api/videolinks/:video_id', function(req, res) {
+    var id = req.params.video_id;
 		Videolink.remove({
-			_id : ObjectId(req.params.video_id)
-		}, function(err, videolink) {
-      console.log(req.params);
-      console.log(ObjectId(req.params.video_id));
-      console.log(videolink);
-			if (err)
+			_id : ObjectId(id)
+			//_id : ObjectId('562b9068633288160d8b4568')
+		}, function(err) {
+
+			if (err) {
 				res.send(err);
+      }
 
 			getLinks(res);
 		});
 	});
 
-	// create todo and send back all todos after creation
-	app.post('/api/todos', function(req, res) {
+	//// create todo and send back all todos after creation
+	//app.post('/api/todos', function(req, res) {
 
-		// create a todo, information comes from AJAX request from Angular
-		Todo.create({
-			text : req.body.text,
-			done : false
-		}, function(err, todo) {
-			if (err)
-				res.send(err);
+	//	// create a todo, information comes from AJAX request from Angular
+	//	Todo.create({
+	//		text : req.body.text,
+	//		done : false
+	//	}, function(err, todo) {
+	//		if (err)
+	//			res.send(err);
 
-			// get and return all the todos after you create another
-			getTodos(res);
-		});
+	//		// get and return all the todos after you create another
+	//		getTodos(res);
+	//	});
 
-	});
-
-	// delete a todo
-	app.delete('/api/todos/:todo_id', function(req, res) {
-		Todo.remove({
-			_id : req.params.todo_id
-		}, function(err, todo) {
-			if (err)
-				res.send(err);
-
-			getTodos(res);
-		});
-	});
+	//});
 
 	// application -------------------------------------------------------------
-	app.get('*', function(req, res) {
-		//res.sendFile('./public/index.html'); // load the single view file (angular will handle the page changes on the front-end)
-		res.sendFile('index.html', {root: path.join(__dirname, './public')}); // load the single view file (angular will handle the page changes on the front-end)
+	app.get('*', function(res) {
+		res.sendFile('index.html', {root: path.join(__dirname, './public')}); // load the single view file 
 	});
 };
