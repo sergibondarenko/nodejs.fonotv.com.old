00324545d6e541a53f30855e704b529f69a6d70c
Sergey Bondarenko <sergibondarenko@gmail.com>
2016-04-06 23:55:22 +0200

    Correct ident.

--- app/routes.js
+++ app/routes.js
@@ -2,52 +2,48 @@ var path = require('path');
 var Videolink = require('./models/fonotvdb');
 
 var mongodb = require('mongodb');
-var ObjectId = mongodb.ObjectID;
 
 // Get links
 function getLinks(res){
-	Videolink.find(function(err, hyperlinks) {
-
-			// if there is an error retrieving, send the error. nothing after res.send(err) will execute
-			if (err) {
-				res.send(err);
-      }
-
-			res.json(hyperlinks); // return all videos in JSON format
-		});
+  Videolink.find(function(err, hyperlinks) {
+    // if there is an error retrieving, send the error. nothing after res.send(err) will execute
+    if (err) {
+    	res.send(err);
+    }
+    
+    res.json(hyperlinks); // return all videos in JSON format
+  });
 }
 
 module.exports = function(app) {
 
 	// api ---------------------------------------------------------------------
 
+  // use mongoose to get all videos in the database
 	app.get('/api/videolinks', function(req, res) {
-
-		// use mongoose to get all videos in the database
 		getLinks(res);
 	});
 
 	// delete a video
 	app.delete('/api/videolinks/:video_id', function(req, res) {
-    var id = req.params.video_id;
 		Videolink.remove({
-			_id : ObjectId(id)
-			//_id : ObjectId('562b9068633288160d8b4568')
+			_id : mongodb.ObjectID(req.params.video_id)
 		}, function(err) {
 
 			if (err) {
 				res.send(err);
       }
+      console.log("Removed id= " + req.params.video_id);
 
 			getLinks(res);
 		});
 	});
 
 	//// create todo and send back all todos after creation
-	//app.post('/api/todos', function(req, res) {
+	//app.post('/api/videolinks', function(req, res) {
 
 	//	// create a todo, information comes from AJAX request from Angular
-	//	Todo.create({
+	//	Videolink.create({
 	//		text : req.body.text,
 	//		done : false
 	//	}, function(err, todo) {
