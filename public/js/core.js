e35d6cf2dfd93be3d2e24a938901f1141423d329
Sergey Bondarenko <sergibondarenko@gmail.com>
2016-06-04 17:54:54 +0000

    Delete commented code.

--- public/js/core.js
+++ public/js/core.js
@@ -1,3 +1,2 @@
 /*global angular*/
 angular.module('fonotv', ['videoloopController', 'fonotvService']);
-//angular.module('fonotv', ['fonotvController', 'videoloopController', 'videoloop_blackController', 'fonotvService']);
